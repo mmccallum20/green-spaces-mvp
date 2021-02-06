43c69e6e9b70cbd3a8c454dfe2524a5ce6d54637
mmccallum20 <mccallummsc@gmail.com>
2021-02-06 21:42:08 +0000

    Form created with text field and buttons, data passing correctly when submit button pressed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,47 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [postcode, setPostcode] = useState("");
+
+  // The change is effected using a switch statement.
+  // When text is inputted, a value is set to the name
+
+  const handleInputChange = (event) => {
+    let { name, value } = event.target;
+
+    switch (name) {
+      case "postcode":
+        setPostcode(value);
+        break;
+      default:
+    }
+  };
+
+  const handleSubmit = (event) => {
+    console.log(postcode); // Just to check if button works
+    event.preventDefault(); // Do not send to server
+    // let newPostcode = { postcode }; // defining a newProject
+    // props.addPostcode(newPostcode); // add the project using props
+    setPostcode("");
+  };
+
   return (
     <div className="App">
-      <h1>This is the first page.</h1>
+      <h1>Welcome to Green Spaces</h1>
+      <h2>Enter your UK postcode below to find a local green space.</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="postcode">Postcode</label>
+        <input
+          name="postcode"
+          id="postcode"
+          // Curly brackets here indicate to substitute javascript in here (from above - see line 6, variable)
+          value={postcode}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
