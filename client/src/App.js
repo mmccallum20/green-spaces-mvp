5bab44eff365cf8de02a7236588d9de102e55538
mmccallum20 <mccallummsc@gmail.com>
2021-02-16 03:34:21 +0000

    Mapbox integration done, map showing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PostcodeForm from "./components/PostcodeForm";
 import MapData from "./components/MapData";
+import ReactMapGL from "react-map-gl";
 import "./App.css";
 
 const BASE_URL = "https://api.postcodes.io/postcodes/";
@@ -10,6 +11,13 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [mapDetails, setMapDetails] = useState(null);
   const [error, setError] = useState("");
+  const [viewport, setViewport] = useState({
+    latitude: 51.509865,
+    longitude: -0.118092,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
 
   async function pause(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -52,6 +60,16 @@ function App() {
       )}
 
       {error && <h3>{error}</h3>}
+
+      <ReactMapGL
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        {...viewport}
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        Marker Here
+      </ReactMapGL>
     </div>
   );
 }
