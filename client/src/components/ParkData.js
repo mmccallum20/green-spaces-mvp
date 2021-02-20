1bcbd24a6733bfdbdfa005f7752de3daccf78fe4
mmccallum20 <mccallummsc@gmail.com>
2021-02-20 01:29:08 +0000

    selected parks now being pushed into array of objects

--- client/src/components/ParkData.js
+++ client/src/components/ParkData.js
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
 
 function ParkData(props) {
-  // const [parkName, setParkName] = useState("");
-  // const [parkAddress, setParkAddress] = useState("");
-
   let p = props.parkDetails;
-  console.log(p);
-
-  // console.log(props.parkDetails);
-
   let parkArray = [];
+  let selectedParkArray = [];
 
   for (let i = 0; i <= 4; i++) {
     if (!p.features[i].properties.category) {
@@ -19,30 +13,34 @@ function ParkData(props) {
     if (!values) {
       continue;
     } else if (values === "park, leisure" || "picnic") {
-      parkArray.push(p.features[i].text);
+      //parkArray.push(p.features[i].text);
       parkArray.push(p.features[i].place_name);
+      let selectedParkName = p.features[i].place_name;
+      let selectedParkLatitude = p.features[i].geometry.coordinates[1];
+      let selectedParkLongitude = p.features[i].geometry.coordinates[0];
+      selectedParkArray.push({
+        selectedParkName,
+        selectedParkLatitude,
+        selectedParkLongitude,
+      });
     }
   }
 
-  console.log(parkArray);
+  console.log(selectedParkArray);
 
   ///props.parkDetails.features[3].properties.category === "parks, leisure"
 
   return (
     <div className="ParkData">
       <h2>Your top green spaces are:</h2>
-      <h3>{parkArray[0]}</h3>
-      <h4>{parkArray[1]}</h4>
-      <h3>{parkArray[2]}</h3>
-      <h4>{parkArray[3]}</h4>
-      <h3>{parkArray[4]}</h3>
-      <h4>{parkArray[5]}</h4>
-      <h3>{parkArray[6]}</h3>
-      <h4>{parkArray[7]}</h4>
-      <h3>{parkArray[8]}</h3>
-      <h4>{parkArray[9]}</h4>
-
-      {/* There must be a better way of doing this - research.  */}
+      <div>
+        <ul>
+          {/* Later - see if we can use map on selectedParkArray to avoid repetition */}
+          {parkArray.map((name) => (
+            <li key={name}> {name} </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
